b47163bfebc95f3f12b797be3fe9c961090ee42f
Aldair Carrillo <alda90dev@gmail.com>
2024-03-02 14:37:22 -0700

    Fix tournament validations

--- routes/tournament.js
+++ routes/tournament.js
@@ -8,9 +8,7 @@ const router = Router();
 router.post('/new', [
     check('name', 'El nombre es obligatorio').not().isEmpty(),
     check('name_en', 'El nombre en ingles es obligatorio').not().isEmpty(),
-    check('image', 'La imagen es obligatoria').not().isEmpty(),
-    check('image2', 'La imagen2 es obligatoria').not().isEmpty(),
-    check('conferation_id', 'El id del torneo es obligatorio').not().isEmpty(),
+    check('conferation_id', 'El id de la confederacion es obligatorio').not().isEmpty(),
     validateFields
 ], validateJWT, createTournament);
 
@@ -18,9 +16,7 @@ router.post('/update', [
     check('_id', 'El id es obligatorio').not().isEmpty(),
     check('name', 'El nombre es obligatorio').not().isEmpty(),
     check('name_en', 'El nombre en ingles es obligatorio').not().isEmpty(),
-    check('image', 'La imagen es obligatoria').not().isEmpty(),
-    check('image2', 'La imagen2 es obligatoria').not().isEmpty(),
-    check('conferation_id', 'El id del torneo es obligatorio').not().isEmpty(),
+    check('conferation_id', 'El id de la confederacion es obligatorio').not().isEmpty(),
     validateFields
 ], validateJWT, updateTournament);
 
